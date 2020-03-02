e80476dad25cabf5a5ef2ea8cddc43a062929fe1
Dany <53636771+Dany-LF@users.noreply.github.com>
2020-03-02 17:50:36 +0100

    Proposal
    Add a condition for verify if the langage exist or no

--- index.js
+++ index.js
@@ -25,10 +25,14 @@ client.on("message", (message) => {
     }
     
     if(message.content.startsWith("!setlang")){
+        var langages = ["english", "french"]
         const newLanguageName = message.content.split(" ")[1];
         // If no new language is specified, returns an error message
         if(!newLanguageName){
             return message.channel.send(language("MISSING_LANGUAGE"));
+        }
+        if(!langages.includes(newLanguageName)){
+            return message.channel.send(language("LANGUAGE_NO_EXIST"));
         }
         // Update our json database
         guildLanguages[message.guild.id] = newLanguageName;
